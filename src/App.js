63e22d1ea6b363f8f5ff1e8c8d0514640d942f17
Benson-Kim <bensonkimkam@gmail.com>
2022-10-05 22:04:15 +0300

    cleaned files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-import Layout from "./components/Layout";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
 
-import Dashboard from "./pages/Dashboard";
-import Products from "./pages/Products/Products";
-import Orders from "./pages/Orders/Orders";
-import Customers from "./pages/Customers/Customers";
-import Settings from "./pages/Settings/Settings";
-import Messages from "./pages/Messages";
-
-function App() {
-	return (
-		<BrowserRouter>
-			<Layout>
-				<Routes>
-					<Route path='/' element={<Dashboard />} />
-					<Route path='/orders' element={<Orders />} />
-					<Route path='/products' element={<Products />} />
-					<Route path='/customers' element={<Customers />} />
-					<Route path='/messages' element={<Messages />} />
-					<Route path='/settings' element={<Settings />} />
-				</Routes>
-			</Layout>
-		</BrowserRouter>
-	);
-}
+const App = () => {
+	return <div>App</div>;
+};
 
 export default App;
